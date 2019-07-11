3f82e21adfea5543590b7e825195431df5bae88b
Tawhid Ali <tawhidali126@gmail.com>
2019-07-11 23:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
 
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
