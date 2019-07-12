b890aa2a9c05a3dcf88dd30aaa690dd3c4ceeff9
Tawhid Ali <tawhidali126@gmail.com>
2019-07-12 00:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === string.toLowerCase) {
+  if(string == string.toLowerCase) {
     const grandReply = 'I can\'t hear you!';
     return grandReply;
   }
